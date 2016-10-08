7eb96cebb7cc91e305a2442f17334a529f3b5a67
ferrugemjs <ferrugemjavascript@gmail.com>
2016-10-08 23:32:52 +0000

    new process on compile

--- src/desktop/main/main.ts
+++ src/desktop/main/main.ts
@@ -13,7 +13,8 @@ export class Main{
 		//this.refresh();
 		systemConfigDispatch.dispatchChangeBackgroundImage.subscribe((bckimg)=>{
 			this.selectedBackgroundImage = bckimg;
-			//document.getElementsByClassName("main-app")[0]
+			let element:any = document.getElementsByClassName("main-app")[0];
+			element.style["background-image"] = "url('dist/desktop/main/assets/img/"+bckimg+"')";
 			//(<any>this).refresh();
 		});
 	}
